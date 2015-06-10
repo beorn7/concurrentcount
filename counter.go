ca13991aa628b9f811e90320c620d5f742e0380b
beorn7 <beorn@soundcloud.com>
2015-06-10 20:30:22 +0200

    Re-add outer loop in channel counter.

--- counter.go
+++ counter.go
@@ -164,11 +164,18 @@ func (c *ChannelCounter) loop() {
 
 	var value float64
 	for {
+		// Outer select: First process all the waiting samples.
+		// This is not ideal as it might let Get block forever.
 		select {
 		case v := <-c.queue:
 			value += v
-		case c.value <- value:
-			// Do nothing.
+		default:
+			select {
+			case v := <-c.queue:
+				value += v
+			case c.value <- value:
+				// Do nothing.
+			}
 		}
 	}
 }
